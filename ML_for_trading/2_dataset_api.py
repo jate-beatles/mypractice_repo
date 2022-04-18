02a41aa13f12e4936f33e29583d3edf2cf17081b
jate-beatles <jate@live.cn>
2022-04-18 23:38:11 +0200

    dataset_create

--- ML_for_trading/2_dataset_api.py
+++ ML_for_trading/2_dataset_api.py
@@ -64,4 +64,40 @@ assert abs(w0 -2) < 0.001
 assert abs(w1 - 10) < 0.001
 
 
+CSV_COLUMNS = [
+    "fare_amount",
+    "pickup_datetime",
+    "pickup_longitude",
+    "pickup_latitude",
+    "dropoff_longitude",
+    "dropoff_latitude",
+    "passenger_count",
+    "key",
+]
+LABEL_COLUMN = 'fare_amount'
+DEFAULTS = [[0.0], ['na'], [0.0], [0.0],[0.0],[0.0],[0.0],['na']]
+UNWATED_COLS = ['pickup_datetime', 'key']
+
+def features_and_labels(row_data):
+    label = row_data.pop(LABEL_COLUMN) ##removed the NULL data as DEFAULTS  
+    features = row_data
+
+    for unwated_col in UNWATED_COLS:
+        features.pop(unwated_col)
+    return features, label 
+
+##tf.data.experimetal.make_csv_dataset() method reads CSV files into a dataset
+def creat_dataset(pattern, batch_size=1, mod="eval"):
+    dataset =tf.data.experimental.make_csv_dataset(
+        pattern, batch_size, CSV_COLUMNS, DEFAULTS 
+    )
+    dataset = dataset.map(features_and_labels)
+
+    if mode =="train":
+        dataset = dataset.shuflle(buffer_size = 1000).repeat()
+    dataset = dataset.prefetch(1) ##take advantage of multi-threading; 1= autotune
+    return dataset 
+
+
+
 
